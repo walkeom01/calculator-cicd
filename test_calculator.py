3e0fa81cb1028ec9193fb2f7100093c66ff58413
om <walkeom860@gmail.com>
2026-09-03 19:04:07 +0530

    initial calculator project

---
+++ test_calculator.py
@@ -0,0 +1,13 @@
+from calculator import add, subtract, multiply, divide
+
+def test_add():
+    assert add(2, 3) == 5
+
+def test_subtract():
+    assert subtract(5, 2) == 3
+
+def test_multiply():
+    assert multiply(3, 4) == 12
+
+def test_divide():
+    assert divide(10, 2) == 5
